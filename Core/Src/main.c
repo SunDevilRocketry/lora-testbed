a88a82685434c26f734060bc3c00afcc8fbd75f0
niekky <trungkiencfc@gmail.com>
2025-09-26 09:11:22 -0700

    feat: added lora

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,7 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
+#include "lora.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
